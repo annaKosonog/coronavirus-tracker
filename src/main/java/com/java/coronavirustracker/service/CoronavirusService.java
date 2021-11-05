1204a079283d81ac579d864b0354138f6dabdacf
annaKosonog <anna.kosonog21@gmail.com>
2021-11-05 18:15:50 +0100

    "Method to parse csv data"

--- src/main/java/com/java/coronavirustracker/service/CoronavirusService.java
+++ src/main/java/com/java/coronavirustracker/service/CoronavirusService.java
@@ -2,6 +2,8 @@ package com.java.coronavirustracker.service;
 
 
 import com.java.coronavirustracker.model.LocationStat;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,16 +23,21 @@ import java.util.List;
 @Service
 public class CoronavirusService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private List<LocationStat> allStats = new ArrayList<>();
+
+    public List<LocationStat> getAllStats() {
+        return allStats;
+    }
 
     @PostConstruct
     @Scheduled(cron = "* * 1 * * * ")
-    public void fetchVirusData() {
+    public void fetchVirusData() throws IOException {
         List<LocationStat> newStats = new ArrayList<>();
 
         HttpGet httpGet = new HttpGet(VIRUS_DATA_URL);
         HttpClient httpClient = HttpClientBuilder.create().build();
         HttpResponse httpResponse = null;
-        String responseBody= null;
+        String responseBody = null;
         try {
             httpResponse = httpClient.execute(httpGet);
         } catch (IOException e) {
@@ -41,6 +49,18 @@ public class CoronavirusService {
             e.printStackTrace();
             System.out.println(responseBody);
         }
-    }
 
+        StringReader csvBodyReader = new StringReader(responseBody);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            LocationStat locationStat = new LocationStat();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStat);
+            newStats.add(locationStat);
+        }
+        this.allStats = newStats;
+    }
 }
+
